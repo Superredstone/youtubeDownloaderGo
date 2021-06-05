11286e0e4b17d1e1b0e44a4054c24e36aa30deac
Superredstone <patrickcanal3@gmail.com>
2021-06-05 16:47:12 +0200

    Old library completely deleted

--- example.go
+++ example.go
@@ -3,13 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
-	ytDownloader "github.com/Superredstone/youtubeDownloaderGo/Lib"
+	ytDownloader "github.com/Superredstone/youtubeDownloaderGo/pkg"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("./YoutubeDownloaderGo -u <Video URL> -o <FileName>")
+		os.Exit(1)
+	}
+
 	url := flag.String("u", "", "Youtube video URL")
 	out := flag.String("o", "", "Output name")
+
+	if *out == "" {
+		*out = "Download"
+	}
 
 	flag.Parse()
 
--- v2/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Superredstone/youtubeDownloaderGo
-
-go 1.16
